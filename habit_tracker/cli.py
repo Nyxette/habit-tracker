a6099db788dd7fa636c39e7159ecb4d320737cf5
Nav <pranavmalviya2022@gmail.com>
2026-04-22 00:14:54 +0530

    added stats commands with some streaks

--- habit_tracker/cli.py
+++ habit_tracker/cli.py
@@ -1,11 +1,89 @@
 import typer
 from rich.console import Console
 from db import init_db, get_connection
-from datetime import datetime
+from datetime import datetime,timedelta
+from rich.table import Table
 
 app= typer.Typer()
 console=Console()
 
+
+@app.command()
+def stats(habit:str):
+    """Show stats for habit by name"""
+    conn=get_connection()
+    cursor=conn.cursor()
+    var=cursor.execute("SELECT * from habits where name=?",(habit,)).fetchone()
+    if var:
+        habit_id=var[0]
+        # log_id=cursor.execute("SELECT * from habit_logs where habit_id=?",(habit_id,)).fetchall()
+        today=datetime.now().date()
+
+        #Counts of logs in the last week, day and month
+        week_ago=(today-timedelta(days=7)).isoformat()
+        count_week=cursor.execute("SELECT COUNT(*) from habit_logs where habit_id=? AND DATE(logged_at) >= ?",(habit_id,week_ago)).fetchone()
+        day_ago=(today-timedelta(days=1)).isoformat()
+        count_day=cursor.execute("SELECT COUNT(*) from habit_logs where habit_id=? AND DATE(logged_at) >= ?",(habit_id,day_ago)).fetchone()
+        month_ago=(today-timedelta(days=30)).isoformat()
+        count_month=cursor.execute("SELECT COUNT(*) from habit_logs where habit_id=? AND DATE(logged_at) >= ?",(habit_id,month_ago)).fetchone()
+
+        #Checks streaks
+        dates = cursor.execute("SELECT DISTINCT DATE (logged_at) FROM habit_logs WHERE habit_id=? ORDER BY logged_at DESC",(habit_id,)).fetchall()
+        cur_streak=0
+        yesterdate=today
+        for i in dates:
+            cur_date = datetime.strptime(i[0], "%Y-%m-%d").date()
+            if(cur_date==yesterdate):
+                cur_streak+=1
+                yesterdate=yesterdate-timedelta(days=1)
+            else:
+                break
+        
+        
+        asc_dates = cursor.execute("SELECT DISTINCT DATE (logged_at) FROM habit_logs WHERE habit_id=? ORDER BY logged_at ASC",(habit_id,)).fetchall()
+        current_streak=0
+        best_streak=0
+        last_date=None
+        for i in asc_dates:
+            current_date=datetime.strptime(i[0], "%Y-%m-%d").date()
+            if (last_date==None or current_date==last_date):
+                current_streak+=1
+            elif(current_date==last_date+timedelta(days=1)):
+                current_streak+=1
+            else:
+                best_streak=max(best_streak,current_streak)
+                current_streak=1
+            last_date=current_date
+        best_streak=max(best_streak,current_streak)
+
+        table= Table (title="[green]My Habit Table[/green]")
+        table.add_column("Habit Name")
+        table.add_column("Yesterday")
+        table.add_column("Last Week")
+        table.add_column("Last Month")
+        table.add_column("Current Streak")
+        table.add_column("Best Streak")
+        table.add_row(habit,str(count_day[0]),str(count_week[0]),str(count_month[0]),str(cur_streak),str(best_streak))
+        console.print(table)
+
+    else:
+        console.print(f"[yellow]Habit not found: {habit}[/yellow]")
+    conn.close()
+
+
+@app.command()
+def log(habit:str):
+    """Log a habit by name"""
+    conn=get_connection()
+    cursor=conn.cursor()
+    var=cursor.execute("SELECT * from habits where name=?",(habit,)).fetchone()
+    if var:
+        console.print(f"[green]Logging habit: {habit}[/green]")
+        cursor.execute("INSERT INTO habit_logs (habit_id,logged_at) VALUES (?,?)",(var[0],datetime.now().isoformat()))
+    else:
+        console.print(f"[yellow]Habit not found: {habit}[/yellow]")
+    conn.commit()
+    conn.close()
 
 @app.command()
 def list_habits():
@@ -30,6 +108,7 @@ def delete(habit_to_del:str):
         console.print(f"[yellow]Habit not found: {habit_to_del}[/yellow]")
     conn.commit()
     conn.close()
+
 @app.command()
 def edit(old_name:str,new_name:str):
     """Edit habit's name"""
